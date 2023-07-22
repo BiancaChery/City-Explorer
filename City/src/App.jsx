ef01174cce4b3f431a14889e8ac4b412b825a131
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-07-22 20:13:00 +0000

    added .env file

--- City/src/App.jsx
+++ City/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  handleGetCities = async () => {
+    let result = axios.get("https://us1.locationiq.com/v1/search.php")
+  }
+
   render() {
     return (
-      <h1>City Explorer</h1>
+      <>
+      City Explorer
+      <button onClick={handleGetCities}>get Cities</button>
+      </>
     )
-  }
 }
 
 export default App;
