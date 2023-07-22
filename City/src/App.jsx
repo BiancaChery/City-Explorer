10ef8f267f2552a64d21681564dcfb7397b909ed
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-07-22 22:09:22 +0000

    handle Explore added

--- City/src/App.jsx
+++ City/src/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import axios from 'axios';
+import
 
 let mapKey = import.meta.env.VITE_MAP_API_KEY;
-console.log(mapKey)
+
 
 class App extends React.Component {
   handleGetCities = async () => {
-    let result = axios.get(`https://us1.locationiq.com/v1/search?key=${mapKey}&q=fort%20lauderdale&format=json`)
+    let result = await axios.get(`https://us1.locationiq.com/v1/search?key=${mapKey}&q=fort%20lauderdale&format=json`)
     console.log(result)
+  }
+
+  handleExplore = (event) => {
+    event.preventDefault();
+    console.log("explore", event.target.cityName.value);
   }
 
   render() {
     return (
       <>
-      City Explorer
-      <button onClick={handleGetCities}>Explore!</button>
+      <h1>City Explorer</h1>
+      <form onExplore={this.handleExplore}>    
+      <label> city name: <input type="text" name="cityName"/></label>
+      <button onClick={this.handleGetCities}>Explore!</button>
+      </form>
       </>
-    )}
+    )
+  }
 }
 
 export default App;
