da170c4c06f1eb2d6fd067a479c934c1ec20185a
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-07-22 21:28:34 +0000

    corrected errors

--- City/src/App.jsx
+++ City/src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 
+let mapKey = import.meta.env.VITE_MAP_API_KEY;
+console.log(mapKey)
+
 class App extends React.Component {
   handleGetCities = async () => {
-    let result = axios.get("https://us1.locationiq.com/v1/search.php")
+    let result = axios.get(`https://us1.locationiq.com/v1/search?key=${mapKey}&q=fort%20lauderdale&format=json`)
+    console.log(result)
   }
 
   render() {
     return (
       <>
       City Explorer
-      <button onClick={handleGetCities}>get Cities</button>
+      <button onClick={handleGetCities}>Explore!</button>
       </>
-    )
+    )}
 }
 
 export default App;
